7b0bdf04067445eb8f527435d32569e2d1af1e3c
harshita <hk389@njit.edu>
2018-03-29 09:26:17 -0400

    7.  Create a controller, route, and view to show a contact form and add it to the main menu of your site.

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="contact">
+        <h1>Contact Page</h1>
+        <p class="lead">
+            This is my Contact Page
+        </p>
+    </div>
+@endsection
